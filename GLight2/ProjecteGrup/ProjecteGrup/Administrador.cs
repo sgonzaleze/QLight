b8d1bb3980c47a4f5208d628829cba048243d79f
Josep-Guiu <jguius2021@cepnet.net>
2021-11-03 22:33:12 +0100

    falta crear form para crear admins

---
+++ GLight2/ProjecteGrup/ProjecteGrup/Administrador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjecteGrup
+{
+    class Administrador
+    {
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String Password { get; set; }
+
+        public String UserName { get; set; }
+
+        public Administrador() { }
+        public Administrador(String name, String surname, String password, String username) {
+            this.Name = name;
+            this.Surname = surname;
+            this.Password = password;
+            this.UserName = username;
+        }
+
+    }
+}
