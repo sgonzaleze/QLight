16e0bab8196e34faca47c0b600083c89240620a9
Josep-Guiu <jguius2021@cepnet.net>
2021-11-07 18:57:31 +0100

    objeto avatar

---
+++ GLight2/ProjecteGrup/ProjecteGrup/Avatar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjecteGrup
+{
+    class Avatar
+    {
+        public int Puntuacion { get; set; }
+        public String Gender { get; set; }
+    }
+}
